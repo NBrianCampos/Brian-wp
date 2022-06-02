2f53c0bb47b668ec999b4ca2588ade1d4663d7dc
Brian Campos <nick.brian.campos@gmail.com>
2022-06-02 16:34:39 -0500

    Actualizacio
    Actualizacion base de datos + theme

---
+++ brian-wordpress/wp-content/plugins/astra-pro-sites/inc/lib/onboarding/assets/dist/main.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-url'), 'version' => 'bf83160dfb9173c8c9f073fd74f9ccef');
